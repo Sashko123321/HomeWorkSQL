7ddeb9f2037578f9451f4b2cf07a17a0e532517d
Sasha Muzyka <154912849+Sashko123321@users.noreply.github.com>
2025-02-22 16:13:56 +0200

    Update HW_1.sql

--- HW_1.sql
+++ HW_1.sql
@@ -1,34 +1,32 @@
 CREATE DATABASE Academy;
-
 USE Academy;
 
 CREATE TABLE Groups (
-    Id INT PRIMARY KEY IDENTITY,
-    Name VARCHAR(10),
-    Rating INT,
-    [Year] INT
+    Id INT PRIMARY KEY IDENTITY NOT NULL,
+    [Name] VARCHAR(10) NOT NULL UNIQUE CHECK ([Name] != ''),
+    Rating INT NOT NULL CHECK (Rating >= 0 AND Rating <= 5),
+    [Year] INT NOT NULL CHECK ([Year] >= 1 AND [Year] <= 5)
 );
 
 CREATE TABLE Departments (
-    Id INT PRIMARY KEY IDENTITY,
-    Financing MONEY,
-    Name VARCHAR(100)
+    Id INT PRIMARY KEY IDENTITY NOT NULL,
+    Financing MONEY NOT NULL CHECK (Financing >= 0),
+    [Name] VARCHAR(100) NOT NULL UNIQUE CHECK ([Name] != '')
 );
 
 CREATE TABLE Faculties (
-    Id INT PRIMARY KEY IDENTITY,
-    Name VARCHAR(100)
+    Id INT PRIMARY KEY IDENTITY NOT NULL,
+    Name VARCHAR(100) NOT NULL UNIQUE CHECK ([Name] != '')
 );
 
 CREATE TABLE Teachers (
-    Id INT PRIMARY KEY IDENTITY,
-    EmploymentDate DATE,
-    Name VARCHAR(50),
-    Surname VARCHAR(50),
-    Premium MONEY,
-    Salary MONEY
+    Id INT PRIMARY KEY IDENTITY NOT NULL,
+    EmploymentDate DATE NOT NULL CHECK (EmploymentDate >= '1990-01-01'),
+    [Name] VARCHAR(32) NOT NULL CHECK ([Name] != ''),
+    Surname VARCHAR(32) NOT NULL CHECK (Surname != ''),
+    Premium MONEY NOT NULL CHECK (Premium >= 0),
+    Salary MONEY NOT NULL CHECK (Salary > 0)
 );
-
 
 
 select * from Groups
